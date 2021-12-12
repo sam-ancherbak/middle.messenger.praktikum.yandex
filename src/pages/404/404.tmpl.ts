fd5c7c4d5e05fa8b6bfcad84ad8dc6ac01b6f0b9
s.ancherbak <s.ancherbak@b2b-center.ru>
2021-12-12 17:10:21 +0400

    sprint_2: 404 и 500 теперь на TS

--- src/pages/404/404.tmpl.ts
+++ src/pages/404/404.tmpl.ts
@@ -1,19 +1,18 @@
-export const internalErrorPage = `
+export const notFoundTemplate: string = `
 <div id="root">
     <div class="content-container">
         <div class="centered-container">
             <div class="error-page_container">
                 <div class="error-page_code">
-                    <span>500</span>
+                    <span>404</span>
                 </div>
                 <div class="error-page_text">
                     <span>
-                        Внутренняя ошибка сервера<br>
-                        Пожалуйста, подождите, уже работаем над этим
+                        Страница не найдена.<br>Возможно, она была перемещена,<br>или вы неверно указали адрес страницы
                     </span>
                 </div>
                 <div class="error-page_img">
-                    <img src="{{errorImageUrl}}" alt="">
+                    <img src="{{this.errorImageUrl}}" alt="">
                 </div>
             </div>
         </div>
--- src/pages/500/500.tmpl.ts
+++ src/pages/500/500.tmpl.ts
@@ -1,21 +1,23 @@
-export const notFoundPage = `
+export const internalErrorTemplate: string = `
 <div id="root">
     <div class="content-container">
         <div class="centered-container">
             <div class="error-page_container">
                 <div class="error-page_code">
-                    <span>404</span>
+                    <span>500</span>
                 </div>
                 <div class="error-page_text">
                     <span>
-                        Страница не найдена.<br>Возможно, она была перемещена,<br>или вы неверно указали адрес страницы
+                        Внутренняя ошибка сервера<br>
+                        Пожалуйста, подождите, уже работаем над этим
                     </span>
                 </div>
                 <div class="error-page_img">
-                    <img src="{{this.errorImageUrl}}" alt="">
+                    <img src="{{errorImageUrl}}" alt="">
                 </div>
             </div>
         </div>
     </div>
 </div>
 `
+
